6cd31e030cf99535a14c85e6894f823778ccfc2b
Ankit Yadav <149238766+ankityadav125@users.noreply.github.com>
2025-09-17 21:50:00 +0530

    Add files via upload

---
+++ reverseNumber.java
@@ -0,0 +1,18 @@
+package Programming_Solutions;
+
+import java.util.Scanner;
+
+public class reverseNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number:");
+        int num= sc.nextInt();
+        int rev=0;
+        while (num!=0){
+            rev=rev*10+num%10;
+            num=num/10;
+//            System.out.println(rev);
+        }
+        System.out.println(rev);
+    }
+}
